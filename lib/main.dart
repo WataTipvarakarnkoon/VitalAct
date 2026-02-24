4438412287637f4cb9c6c96238064fb76b9ce661
Pru <223513392+TheRealPru@users.noreply.github.com>
2026-02-25 00:49:59 +0700

    splash screen done

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,19 @@ import 'package:flutter/material.dart';
 import 'screens/welcome.dart';
 import 'screens/login.dart';
 import 'screens/signup.dart';
+import 'screens/splash_screen.dart';
 
-void main() {
-  runApp(const MainApp());
+void main() => runApp(const MainApp());
+
+class AppSize {
+  static late double width;
+  static late double height;
+
+  static void init(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    width = size.width;
+    height = size.height;
+  }
 }
 
 class MainApp extends StatelessWidget {
@@ -15,8 +25,9 @@ class MainApp extends StatelessWidget {
     return MaterialApp(
       theme: ThemeData(fontFamily: 'BalooBhai2'),
       debugShowCheckedModeBanner: false,
-      initialRoute: '/welcome',
+      initialRoute: '/splash',
       routes: {
+        '/splash': (context) => const SplashScreen(),
         '/welcome': (context) => const WelcomePage(),
         '/login': (context) => const LoginPage(),
         '/signup': (context) => const SignupPage(),
---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,179 @@
+import 'package:flutter/material.dart';
+import 'package:vitalact/screens/welcome.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _opacity;
+  late Animation<double> _scaleUp;
+  late Animation<double> _scaleDown;
+  late Animation<double> _scaleFull;
+  late Animation<double> _rotation;
+  late Animation<Offset> _position;
+  late Animation<Offset> _positionLeft;
+  late Animation<double> _textOpacity;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 4000),
+    );
+
+    _position =
+        Tween<Offset>(
+          begin: const Offset(0, .7),
+          end: const Offset(0, 0),
+        ).animate(
+          CurvedAnimation(
+            parent: _controller,
+            curve: const Interval(0.10, 0.17, curve: Curves.easeOut),
+          ),
+        );
+
+    _opacity = Tween<double>(begin: 0, end: 1).animate(
+      CurvedAnimation(
+        parent: _controller,
+        curve: const Interval(0.10, 0.17, curve: Curves.easeIn),
+      ),
+    );
+
+    _scaleUp = Tween<double>(begin: 1.0, end: 1.5).animate(
+      CurvedAnimation(
+        parent: _controller,
+        curve: const Interval(0.25, 0.29, curve: Curves.easeOut),
+      ),
+    );
+
+    _rotation = Tween<double>(begin: 0, end: 0.5).animate(
+      CurvedAnimation(
+        parent: _controller,
+        curve: const Interval(0.25, 0.29, curve: Curves.easeIn),
+      ),
+    );
+
+    _scaleDown = Tween<double>(begin: 1.5, end: 0.2).animate(
+      CurvedAnimation(
+        parent: _controller,
+        curve: const Interval(0.35, 0.41, curve: Curves.easeIn),
+      ),
+    );
+
+    _positionLeft =
+        Tween<Offset>(
+          begin: const Offset(0, 0),
+          end: const Offset(4.5, -2.7),
+        ).animate(
+          CurvedAnimation(
+            parent: _controller,
+            curve: const Interval(0.45, 0.62, curve: Curves.easeInOut),
+          ),
+        );
+
+    _scaleFull = Tween<double>(begin: 1.0, end: 50).animate(
+      CurvedAnimation(
+        parent: _controller,
+        curve: const Interval(0.8, 1, curve: Curves.easeOut),
+      ),
+    );
+
+    _textOpacity = Tween<double>(begin: 0, end: 1).animate(
+      CurvedAnimation(
+        parent: _controller,
+        curve: const Interval(0.5, 0.65, curve: Curves.easeIn),
+      ),
+    );
+
+    _controller.forward();
+
+    Future.delayed(const Duration(seconds: 5), () {
+      if (!mounted) return;
+
+      Navigator.of(context).pushReplacement(
+        PageRouteBuilder(
+          transitionDuration: const Duration(milliseconds: 600),
+          pageBuilder: (context, animation, secondaryAnimation) =>
+              const WelcomePage(),
+          transitionsBuilder: (context, animation, secondaryAnimation, child) {
+            return FadeTransition(opacity: animation, child: child);
+          },
+        ),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Stack(
+          alignment: Alignment.center,
+          children: [
+            AnimatedBuilder(
+              animation: _controller,
+              builder: (context, child) {
+                return Opacity(
+                  opacity: _opacity.value,
+                  child: Transform.scale(
+                    scale: _scaleUp.value,
+                    child: SlideTransition(
+                      position: _position,
+                      child: Transform.rotate(
+                        angle: _rotation.value,
+                        child: Transform.scale(
+                          scale: _scaleDown.value,
+                          child: SlideTransition(
+                            position: _positionLeft,
+                            child: Transform.scale(
+                              scale: _scaleFull.value,
+                              child: child,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                );
+              },
+              child: Container(
+                height: 80,
+                width: 80,
+                decoration: BoxDecoration(
+                  color: Color(0xFFFF4646),
+                  borderRadius: BorderRadius.circular(5),
+                ),
+              ),
+            ),
+
+            const SizedBox(height: 20),
+
+            Transform.translate(
+              offset: Offset(-30, 0),
+              child: FadeTransition(
+                opacity: _textOpacity,
+                child: const Text(
+                  'VitalAct',
+                  style: TextStyle(
+                    color: Color(0xFFFF4646),
+                    fontFamily: 'BalooBhai2',
+                    fontSize: 70,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
