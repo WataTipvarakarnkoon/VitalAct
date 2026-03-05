c3a265c68a2f1bb16ee13c137e067e59574458c1
Wata Tipvarakarnkoon <106424228+WataTipvarakarnkoon@users.noreply.github.com>
2026-03-05 09:06:31 +0700

    fix error message on empty form

--- lib/utils/validators.dart
+++ lib/utils/validators.dart
@@ -1,10 +1,6 @@
 class Validators {
   static String? email(String? value) {
-    if (value == null || value.isEmpty) {
-      return "Email cannot be empty";
-    }
-
-    if (!value.contains('@')) {
+    if (!value!.contains('@')) {
       return "Enter a valid email";
     }
 
@@ -12,11 +8,7 @@ class Validators {
   }
 
   static String? password(String? value) {
-    if (value == null || value.isEmpty) {
-      return "Password cannot be empty";
-    }
-
-    if (value.length < 6) {
+    if (value!.length < 6) {
       return "Password must be at least 6 characters";
     }
 
