09b75ed48088cf5ddaa8ba875580a434d83c3f00
Wata Tipvarakarnkoon <106424228+WataTipvarakarnkoon@users.noreply.github.com>
2026-03-08 00:07:19 +0700

    add unity project widget but error

--- android/app/src/main/kotlin/com/example/vitalact/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/vitalact/MainActivity.kt
@@ -2,4 +2,6 @@ package com.example.vitalact
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity()
++ import com.xraph.plugin.flutter_unity_widget.FlutterUnityActivity;
+
++ class MainActivity: FlutterUnityActivity()
--- lib/screens/main/test_unity.dart
+++ lib/screens/main/test_unity.dart
@@ -1,2 +1,30 @@
 import 'package:flutter_unity_widget/flutter_unity_widget.dart';
 import 'package:flutter/material.dart';
+
+class TestUnity extends StatefulWidget {
+  const TestUnity({Key? key}) : super(key: key);
+
+  @override
+  State<TestUnity> createState() => _TestUnityState();
+}
+
+class _TestUnityState extends State<TestUnity> {
+  UnityWidgetController? _unityWidgetController;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        color: Colors.yellow,
+        child: UnityWidget(
+          onUnityCreated: onUnityCreated,
+        ),
+      ),
+    );
+  }
+
+  // Callback that connects the created controller to the unity controller
+  void onUnityCreated(UnityWidgetController controller) {
+    _unityWidgetController = controller;
+  }
+}
