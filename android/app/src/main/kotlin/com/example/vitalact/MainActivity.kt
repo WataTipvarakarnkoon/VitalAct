5f43b51d37223081b6aae0880b2e57d613426a1b
Tinnakrit <223513392+TheRealPru@users.noreply.github.com>
2026-03-09 02:10:43 +0700

    idk

--- android/app/src/main/kotlin/com/example/vitalact/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/vitalact/MainActivity.kt
@@ -2,6 +2,5 @@ package com.example.vitalact
 
 import io.flutter.embedding.android.FlutterActivity
 
-+ import com.xraph.plugin.flutter_unity_widget.FlutterUnityActivity;
-
-+ class MainActivity: FlutterUnityActivity()
+class MainActivity: FlutterUnityActivity() {
+}
